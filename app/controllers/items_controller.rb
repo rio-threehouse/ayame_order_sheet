f1d7e603e843a90ae2bbba6c86ac94d5c5287b5f
Rio Sanka <threehouse.second@gmail.com>
2019-08-20 15:04:35 +0900

    add item create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(params_item)
+    if @item.save
+      flash[:success] = 'itemを登録しました'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'itemの登録に失敗しました'
+      render :new
+    end
   end
 
   def destroy
@@ -15,5 +24,11 @@ class ItemsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+  
+  def params_item
+    params.require(:item).permit(:number, :name, :color_1, :color_2, :color_3, :color_4, :size_1, :size_2, :size_3, :size_4, :price)
   end
 end
